d51f55f9d97ca35c6eb2148eb427297fa22f660e
iamashubajaj <106901499+iamashubajaj@users.noreply.github.com>
2023-05-20 18:18:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function generateWish() {
     var name = document.getElementById("nameInput").value;
-    var message = "Happy birthday, " + name + "! May all your wishes come true. Have a fantastic day!";
+    var message = "Happy birthday, " + name + "! May all your wishes come true. Have a fantastic day!\nFrom Ashutosh";
     document.getElementById("birthdayMessage").textContent = message;
 }
